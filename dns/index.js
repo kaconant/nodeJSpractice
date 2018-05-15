8741c445b2ae4f2e672e7fe0e70b470775a83aeb
Krissy Conant <kconant@KConant-MacBook-Pro.local>
2018-05-15 19:11:44 -0400

    updated notes per L

--- dns/index.js
+++ dns/index.js
@@ -1,16 +1,34 @@
 const dns = require('dns');
 const readline = require('readline');
 
-
+// create interface
 const rl = readline.createInterface({
    input: process.stdin,
    output: process.stdout
 });
 
+const lookup = util.promisify(dns.lookup);
+
+lookup(domain)
+.then((address) => {
+    console.log(`IP address: ${address}`);
+})
+.catch((err) => {
+return console.log(err)
+});
+
+/* OR
+
 rl.question('Provide Website: ', (answer) =>{
    rl.close();
+
+   // do the dns lookup
    dns.lookup(answer, (err, address)=>{
        if(err) {return console.log(err)};
        console.log(address);
    })
 })
+
+   */
+
+
